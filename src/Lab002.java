f954291d940e49e29533cfb4ade57e041d8a1a04
Navni <navniverma21@gmail.com>
2023-12-29 20:06:39 +0530

    Data types

---
+++ src/Lab002.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        byte age = 127;
+        int i= 2090;
+        float j= 8090.80f;
+        System.out.println(age+1);
+        System.out.println(i*20.20);
+        System.out.println(j%2);
+    }
+}
